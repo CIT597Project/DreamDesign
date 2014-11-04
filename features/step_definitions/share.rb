c7b8cbf08edd03d822ffb7e4cbc4328ffe072dfd
chan0723 <chanli@seas.upenn.edu>
2014-11-04 07:47:08 -0500

    .feature

---
+++ features/step_definitions/share.rb
@@ -0,0 +1,19 @@
+Given(/^I'm editing the comments$/) do
+  
+end
+
+Given(/^I've logged in with facebook account$/) do
+
+end
+
+When(/^I chcek "(.*?)"$/) do |arg1|
+  
+end
+
+When(/^click save$/) do
+  
+end
+
+Then(/^I can share the comment on Facebook$/) do
+  
+end
---
+++ features/step_definitions/signup.rb
@@ -0,0 +1,63 @@
+Given(/^I'm on the home page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" on the right corner$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I'm on the email sign up page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill eligible email and passwords$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^click "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should sign in my account$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^see my school list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I'm on the sign up page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^there is empty field$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see hint message$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I input an existing email$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I input different passwords$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see error message$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I'm on Facebook sign up page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill correct Facebook email and password$/) do
+  pending # express the regexp above with the code you wish you had
+end
