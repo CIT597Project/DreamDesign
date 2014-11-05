eaf0245c0f9e32b821b8e9219de726f668b0d9c7
lejunqi <qilejun223@gmail.com>
2014-11-04 22:39:47 -0500

    try first commit

--- features/step_definitions/signup.rb
+++ features/step_definitions/signup.rb
@@ -1,8 +1,11 @@
 Given(/^I'm on the home page$/) do
-  visit "/"
+  visit root_path
 end
 
 Then(/^I should see "(.*?)" on the right corner$/) do |arg1|
   page.should have_content arg1
 end
 
+Given(/^I'm on the email sign up page$/) do 
+  visit root_path
+end
