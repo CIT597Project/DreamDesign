a9e828066546d1d4eb4fb8e552766e80fa88c021
chan0723 <chanli@seas.upenn.edu>
2014-11-05 23:35:32 -0500

    username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106015639) do
+ActiveRecord::Schema.define(version: 20141106034619) do
 
   create_table "my_schools", force: true do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20141106015639) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
