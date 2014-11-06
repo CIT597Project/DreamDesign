3cb6a8c55b1c1c6343fdfc77e91e25684b17caca
chan0723 <chanli@seas.upenn.edu>
2014-11-05 22:44:21 -0500

    sign in icon

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  get 'help' => 'static_pages#help'
   get 'mylist' => 'users#mylist'
-  get 'profile' => 'users#profile'
   
   devise_for :users
-  resources :schoollists
+ 
   resources :my_schools
   
 end
