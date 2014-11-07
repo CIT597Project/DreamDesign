cf22a88d306597d63dbf8953121faa02bcbc59d4
penncourse <tqvcd2014@gmail.com>
2014-11-06 19:13:29 -0500

    facebook_pluginsm

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,10 +3,7 @@
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   provider :facebook, ENV['FACEBOOK_472551706219746'], ENV['FACEBOOK_07ba9ffff86c0bdaebf583b37c730262']
 # end
-if Rails.env == "developement"
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '1528970907342665', '83cdceaf84775e82d39effe38f003929'
-  end
+
 OmniAuth.config.logger = Rails.logger
 # echo $FACEBOOK_472551706219746
 # export FACEBOOK_KEY="472551706219746"
@@ -14,6 +11,11 @@ OmniAuth.config.logger = Rails.logger
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   provider :facebook, FACEBOOK_472551706219746, FACEBOOK_07ba9ffff86c0bdaebf583b37c730262
 # end
+if Rails.env == "developement"
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '1528970907342665', '83cdceaf84775e82d39effe38f003929'
+  end
+else
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '472551706219746', '07ba9ffff86c0bdaebf583b37c730262'
 end
