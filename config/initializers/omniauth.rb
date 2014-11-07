af40c7e0654175c59a18605f74bc13d9bc26e52c
penncourse <tqvcd2014@gmail.com>
2014-11-06 19:09:35 -0500

    facebook_pluginMM

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,16 +3,19 @@
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   provider :facebook, ENV['FACEBOOK_472551706219746'], ENV['FACEBOOK_07ba9ffff86c0bdaebf583b37c730262']
 # end
-
+if Rails.env == "developement"
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '1528970907342665', '83cdceaf84775e82d39effe38f003929'
+  end
 OmniAuth.config.logger = Rails.logger
-
+# echo $FACEBOOK_472551706219746
+# export FACEBOOK_KEY="472551706219746"
+# export FACEBOOK_SECRET="07ba9ffff86c0bdaebf583b37c730262"
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   provider :facebook, FACEBOOK_472551706219746, FACEBOOK_07ba9ffff86c0bdaebf583b37c730262
 # end
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_kEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, '472551706219746', '07ba9ffff86c0bdaebf583b37c730262'
 end
 
-OmniAuth.config.on_failure = Proc.new { |env|
-  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
-}
+end
