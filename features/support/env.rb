ea951db4db629816d2583663267bb62c30407af2
lejunqi <qilejun223@gmail.com>
2014-11-22 16:06:20 -0500

    factory girl  pass~
    ~

--- features/support/env.rb
+++ features/support/env.rb
@@ -52,8 +52,9 @@ end
 # newer version of cucumber-rails. Consider adding your own code to a new file
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
-
+require 'pp'
 require 'cucumber/rails'
+require 'factory_girl_rails'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
--- spec/factories/reviews.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :review do
-    body "MyText"
-user nil
-school nil
-  end
-
-end
