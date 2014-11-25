2639498e524ec73cea41fc3ffeafc565e3cff021
chan0723 <chanli@seas.upenn.edu>
2014-11-24 20:54:02 -0500

    rapidly

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -4,20 +4,17 @@ require 'csv'
   
 
   def ranking
-     @schools=School.all.select(:Institution_ID, :follow, :Institution_Name, :Institution_Address, :Institution_City, :Institution_State, :Institution_Zip,
-   :Institution_Phone, :Institution_Web_Address, :Accreditation_Status).uniq.paginate(page: params[:page])
+     @schools=School.all.paginate(page: params[:page])
   end
   
   def hot_ranking
-    @schools=School.all.select(:Institution_ID, :follow, :Institution_Name, :Institution_Address, :Institution_City, :Institution_State, :Institution_Zip,
-  :Institution_Phone, :Institution_Web_Address, :Accreditation_Status).uniq.order(:follow).reverse_order.paginate(page: params[:page])
+    @schools=School.all.order(:follow).reverse_order.paginate(page: params[:page])
     render 'general/ranking'
   end
     
   
   def index
-    @schools = School.search(params[:search]).select(:Institution_ID, :follow, :Institution_Name, :Institution_Address, :Institution_City, :Institution_State, :Institution_Zip,
-  :Institution_Phone, :Institution_Web_Address, :Accreditation_Status).uniq.order(:Institution_Name).paginate(:page=>params[:page])
+    @schools = School.search(params[:search]).order(:Institution_Name).paginate(:page=>params[:page])
     render 'general/ranking'
   end
  
