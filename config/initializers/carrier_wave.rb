bfab9abe5399085e4181748b382a528b366560f7
penncourse <tqvcd2014@gmail.com>
2014-12-09 01:41:28 -0500

    test oncee again

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,6 +6,6 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['AKIAJG47MRRJEVNQWPQQ'],
       :aws_secret_access_key => ENV['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']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  ENV['dreamdesign']
   end
 end
