46688c82827f6befad2f7d4f8f091ad15ab05f56
lejunqi <qilejun223@gmail.com>
2014-12-17 15:02:33 -0500

    test database

--- app/models/recommendeduniversity.rb
+++ app/models/recommendeduniversity.rb
@@ -1,5 +1,5 @@
 class Recommendeduniversity < ActiveRecord::Base
-  self.table_name = "public.RECOMMENDEDUNIVERSITIES"
+  self.table_name = "DREAMDESIGN.RECOMMENDEDUNIVERSITIES"
   def recommendeduniversity_params
       params.require(:recommendeduniversity).permit(:institution_name, :institution_address, :institution_city, :institution_state, :institution_zip,
       :institution_web_address, :name_abb, :name_abb1)
