a0c8cf61f78d9b98e0b2cfd467f610005981521e
lejunqi <qilejun223@gmail.com>
2014-12-17 18:19:31 -0500

    difflist cucumber

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -440,23 +440,24 @@ end
 
 #difflist
 Given(/^I am on the home page$/) do
+  visit'/'
   page.should have_content('Welcome')
 end
 
 When(/^I click on the major ranking button$/) do
-  pending # express the regexp above with the code you wish you had
+  click_link "Major ranking"
 end
 
 Then(/^I should see top ten CS universities$/) do
-  pending # express the regexp above with the code you wish you had
+  page.should have_content("Major Ranking (computer science)")
 end
 
 When(/^I click on the recommended universities button$/) do
-  pending # express the regexp above with the code you wish you had
+  click_link "Recommended Universities"
 end
 
 Then(/^I should see recommended universities list$/) do
-  pending # express the regexp above with the code you wish you had
+  page.should have_content("Princeton University")
 end
 
 
