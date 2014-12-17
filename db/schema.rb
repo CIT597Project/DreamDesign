32888b7bae7c534cae8600d05401985bd240e386
lejunqi <qilejun223@gmail.com>
2014-12-17 14:22:34 -0500

    development database

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,15 @@
 ActiveRecord::Schema.define(version: 20141210075901) do
 
   create_table "hotuniversites", id: false, force: true do |t|
-    t.string "institution_name",        limit: 200
-    t.string "institution_address",     limit: 200
-    t.string "institution_city",        limit: 20
-    t.string "institution_state",       limit: 20
-    t.string "institution_zip",         limit: 20
-    t.string "institution_phone",       limit: 20
-    t.string "institution_web_address", limit: 20
-    t.string "accreditation_status",    limit: 20
+    t.string  "institution_name",        limit: 200
+    t.string  "institution_address",     limit: 200
+    t.string  "institution_city",        limit: 20
+    t.string  "institution_state",       limit: 20
+    t.string  "institution_zip",         limit: 20
+    t.string  "institution_phone",       limit: 20
+    t.string  "institution_web_address", limit: 20
+    t.string  "accreditation_status",    limit: 20
+    t.integer "follow",                  limit: nil, precision: 20, default: 0, null: false
   end
 
   create_table "majorrankings", primary_key: "name_abb", force: true do |t|
@@ -36,14 +37,14 @@ ActiveRecord::Schema.define(version: 20141210075901) do
   create_table "my_schools", force: true do |t|
     t.string   "name"
     t.text     "comment"
-    t.integer  "user_id",    limit: nil
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "school_id",  limit: nil
+    t.integer  "user_id",    limit: nil, precision: 38
+    t.datetime "created_at", limit: 6,                  null: false
+    t.datetime "updated_at", limit: 6,                  null: false
+    t.integer  "school_id",  limit: nil, precision: 38
   end
 
   add_index "my_schools", ["school_id"], name: "index_my_schools_on_school_id"
-  add_index "my_schools", ["user_id", "created_at"], name: "i_my_sch_use_id_cre_at"
+  add_index "my_schools", ["user_id", "created_at"], name: "ind_73790c3e027eeb81"
   add_index "my_schools", ["user_id"], name: "index_my_schools_on_user_id"
 
   create_table "recommendeduniversities", primary_key: "name_abb", force: true do |t|
@@ -57,29 +58,29 @@ ActiveRecord::Schema.define(version: 20141210075901) do
   end
 
   create_table "relationships", force: true do |t|
-    t.integer  "follower_id", limit: nil
-    t.integer  "followed_id", limit: nil
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.integer  "follower_id", limit: nil, precision: 38
+    t.integer  "followed_id", limit: nil, precision: 38
+    t.datetime "created_at",  limit: 6,                  null: false
+    t.datetime "updated_at",  limit: 6,                  null: false
   end
 
   add_index "relationships", ["followed_id"], name: "i_relationships_followed_id"
-  add_index "relationships", ["follower_id", "followed_id"], name: "i_rel_fol_id_fol_id", unique: true
+  add_index "relationships", ["follower_id", "followed_id"], name: "ind_54d45a8824d381dc", unique: true
   add_index "relationships", ["follower_id"], name: "i_relationships_follower_id"
 
   create_table "reviews", force: true do |t|
     t.text     "body"
-    t.integer  "user_id",    limit: nil
-    t.integer  "school_id",  limit: nil
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer  "user_id",    limit: nil, precision: 38
+    t.integer  "school_id",  limit: nil, precision: 38
+    t.datetime "created_at", limit: 6,                  null: false
+    t.datetime "updated_at", limit: 6,                  null: false
   end
 
   add_index "reviews", ["school_id"], name: "index_reviews_on_school_id"
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "schools", force: true do |t|
-    t.integer  "Institution_ID",          limit: nil
+    t.string   "Institution_ID"
     t.string   "Institution_Name"
     t.string   "Institution_Address"
     t.string   "Institution_City"
@@ -88,9 +89,9 @@ ActiveRecord::Schema.define(version: 20141210075901) do
     t.string   "Institution_Phone"
     t.string   "Institution_Web_Address"
     t.string   "Accreditation_Status"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "follow",                  limit: nil, precision: 38, default: 0, null: false
+    t.datetime "created_at",              limit: 6
+    t.datetime "updated_at",              limit: 6
+    t.decimal  "follow",                            default: 0.0, null: false
     t.string   "program_name"
   end
 
@@ -98,15 +99,15 @@ ActiveRecord::Schema.define(version: 20141210075901) do
     t.string   "email",                                             default: "", null: false
     t.string   "encrypted_password",                                default: "", null: false
     t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", limit: 6
+    t.datetime "remember_created_at",    limit: 6
     t.integer  "sign_in_count",          limit: nil, precision: 38, default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at",     limit: 6
+    t.datetime "last_sign_in_at",        limit: 6
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             limit: 6
+    t.datetime "updated_at",             limit: 6
     t.string   "picture"
     t.string   "username"
     t.string   "provider"
@@ -115,7 +116,7 @@ ActiveRecord::Schema.define(version: 20141210075901) do
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size",       limit: nil, precision: 38
-    t.datetime "avatar_updated_at"
+    t.datetime "avatar_updated_at",      limit: 6
     t.string   "high_school"
     t.string   "college"
     t.string   "grad_university"
